d1695372d8dbcdd7d26993bf57d9fed6e7732e16
EMG114 <egutierrez411@gmail.com>
2016-07-07 14:28:24 -0400

    crazy lights

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -11,13 +11,23 @@ import UIKit
 class ViewController: UIViewController {
     
     var shoppingList: [String] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
+        let quantity = [6, 4, 12, 4]
+        let listItems = ["Bananas", "Apples", "Eggs", "Rolls"]
         
+        shoppingList = makeShoppingList(listItems, quantityOfItems: quantity)
+        print (shoppingList)
         
+    }
+    func makeShoppingList(foodNames: [String], quantityOfItems: [Int]) -> [String] {
+        var list : [String] = []
+        for (index, item) in foodNames.enumerate() {
+            list.append("\(index + 1). \(quantityOfItems[index]) \(item)")
+        }
+        return list
     }
     
     
